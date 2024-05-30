7fc910625edf9ffafe32878068691c21ff486837
Jbassard <jonathanacciarito@gmail.com>
2024-05-30 18:38:32 -0400

    small change made to start scripts

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,10 @@
 // ! Good to go
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/YesChef');
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://jbassard97:4RdylWJwtGsUSMPp@cluster0.crhep98.mongodb.net/YesChef?retryWrites=true&w=majority&appName=Cluster0"
+);
 
 module.exports = mongoose.connection;
