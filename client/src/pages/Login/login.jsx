d8ade65266cb3f3e84478da4a14aee2c2eb5a957
Jbassard <jonathanacciarito@gmail.com>
2024-05-30 19:29:33 -0400

    having difficulty with final build resolving, trying a bunch of things to see what clicks

--- client/src/pages/Login/login.jsx
+++ client/src/pages/Login/login.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../../utils/mutations";
 import Auth from "../../utils/auth";
-import "./Login.css";
+import "./login.css";
 
 export default function Login() {
   const [formState, setFormState] = useState({ email: "", password: "" });
