745fc1c25324774691801e3bcf31d7d68e5fe566
Jbassard <jonathanacciarito@gmail.com>
2024-05-30 22:46:38 -0400

    fiddling with puppeteer

--- server/utils/createSchedulePDF.js
+++ server/utils/createSchedulePDF.js
@@ -1,7 +1,9 @@
 const puppeteer = require("puppeteer");
 
 async function createSchedulePDF(bossAndEmpsArray) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
 
   const htmlContent = generateHtmlContent(bossAndEmpsArray);
