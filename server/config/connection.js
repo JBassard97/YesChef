ebf05f40cf9f3d161ac725311eb255ab277fecf7
Jbassard <jonathanacciarito@gmail.com>
2024-06-01 00:08:58 -0400

    small styling change in dashboard

--- server/config/connection.js
+++ server/config/connection.js
@@ -9,8 +9,7 @@ mongoose.connect(
 
 // mongoose.connect(
 //   process.env.MONGODB_URI ||
-//     "mongodb://127.0.0.1:27017/YesChef" ||
-//     "mongodb+srv://jbassard97:4RdylWJwtGsUSMPp@cluster0.crhep98.mongodb.net/YesChef?retryWrites=true&w=majority&appName=Cluster0"
+//     "mongodb://127.0.0.1:27017/YesChef"
 // );
 
 module.exports = mongoose.connection;
