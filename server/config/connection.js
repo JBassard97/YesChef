201ea39fbe4884d90822e0407fff523d06537750
Jbassard <jonathanacciarito@gmail.com>
2024-05-31 23:58:09 -0400

    small change

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,13 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb://127.0.0.1:27017/YesChef" ||
     "mongodb+srv://jbassard97:4RdylWJwtGsUSMPp@cluster0.crhep98.mongodb.net/YesChef?retryWrites=true&w=majority&appName=Cluster0"
 );
+
+// mongoose.connect(
+//   process.env.MONGODB_URI ||
+//     "mongodb://127.0.0.1:27017/YesChef" ||
+//     "mongodb+srv://jbassard97:4RdylWJwtGsUSMPp@cluster0.crhep98.mongodb.net/YesChef?retryWrites=true&w=majority&appName=Cluster0"
+// );
 
 module.exports = mongoose.connection;
